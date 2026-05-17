4398130d13fe165900545b78404e50d664e93d4d
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-05-17 10:11:06 -0500

    Implement initial database setup and task management functions

---
+++ app_db.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('daymap.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            time TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def add_task(name, time=None):
+    conn = sqlite3.connect("daymap.db")
+    c = conn.cursor()
+    c.exeecute("INSERT INTO tasks (name, time) VALUES (?, ?)", (name, time))
+    conn.commit()
+    conn.close()
+
+def get_tasks():
+    conn = sqlite3.connect("daymap.db")
+    c = conn.cursor()
+    c.execute("SELECT id, name, time FROM tasks")
+    tasks = c.fetchall()
+    conn.close()
+    name = tasks[0][1]
+    time = tasks[0][2]
+    return name, time
